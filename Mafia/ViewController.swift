a9dfdfe7f72c7d7b8f456ef260d1d4d0e9533411
jasondou <jasondou@umich.edu>
2016-12-30 10:50:12 -0500

    Initial Commit

---
+++ Mafia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mafia
+//
+//  Created by Jason Dou on 12/30/16.
+//  Copyright (c) 2016 Jason Dou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MafiaTests/MafiaTests.swift
@@ -0,0 +1,36 @@
+//
+//  MafiaTests.swift
+//  MafiaTests
+//
+//  Created by Jason Dou on 12/30/16.
+//  Copyright (c) 2016 Jason Dou. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MafiaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
